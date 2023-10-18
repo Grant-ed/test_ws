d3187b0b10c26c64613975b16fe582a3f731b945
Grant <grantvanbreda@gmail.com>
2023-10-18 13:55:08 +1000

    moved main test to separate file

--- src/example_2/hardware/include/ros2_control_demo_example_2/arduino_comms.hpp
+++ src/example_2/hardware/include/ros2_control_demo_example_2/arduino_comms.hpp
@@ -157,21 +157,4 @@ private:
     int timeout_ms_;
 };
 
-// main method for testing
-int main()
-{
-  ArduinoComms comms;
-  comms.connect("/dev/ttyACM0", 115200, 1000);
-  std::cout << "Connected: " << comms.connected() << std::endl;
-
-  // test sending 0x13.0x1.0x0.0x0.0x21
-  LibSerial::DataBuffer data_to_send = {0x13, 0x1, 0x0, 0x0, 0x0, 0x21};
-  comms.send_bytes(data_to_send);
-
-  // test sending 0x13.0x1.0xa.0xa.0x18
-  LibSerial::DataBuffer data_to_send_2 = {0x13, 0x1, 0xa, 0xa, 0x18};
-  comms.send_bytes(data_to_send_2);
-}
-
-
 #endif // DIFFDRIVE_ARDUINO_ARDUINO_COMMS_HPP
